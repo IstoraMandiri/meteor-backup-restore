1a68f44687dcbbce9144e8aabacf482207e5a5ea
Chris Hitchcott <hitchcott@gmail.com>
2014-09-07 21:42:10 +0800

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A basic backup solution using mongodump and monogorestore",
-  version: "0.0.0",
+  version: "0.0.1",
   git: "https://github.com/hitchcott/meteor-backup-restore",
   name: 'hitchcott:backup-restore'
 });
